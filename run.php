62fc785c5302ddb1e600d7a59fcfcd77c12af987
citywill <citywill@gmail.com>
2017-09-02 18:55:40 +0800

    update

--- run.php
+++ run.php
@@ -18,7 +18,13 @@ $mark2blog = new lib\mark2blog($m2bConfig);
 $mark2blog->run();
 $generated = $mark2blog->generated;
 
-exec('git commit -am "update"');
-exec('git push origin');
+exec('cd ' . APP_PATH);
+exec('cd ./ && git commit -am "update" && git push origin master');
+
+exec('cd ' . APP_PATH);
+exec('cd ./../citywill.github.io && git commit -am "update" && git push origin master');
+
+exec('cd ' . APP_PATH);
+exec('cd ./../md4blog && git commit -am "update" && git push origin master');
 
 echo '本次执行生成了' . $generated['article'] . '篇文章、' . $generated['index'] . '个索引页、' . $generated['rss'] . '；生成目录为' . $mark2blog->htmlPath;
--- template/default/article.php
+++ template/default/article.php
@@ -28,27 +28,28 @@
 </nav>
 <?php endif?>
 
+<?php /*
 <div class="comment">
-    <div class="page-header">
-        <h3>评论</h3>
-    </div>
+<div class="page-header">
+<h3>评论</h3>
+</div>
 <!-- 多说评论框 start -->
-    <div class="ds-thread" data-thread-key="<?php echo $assign['wholeName'] ?>" data-title="<?php echo $assign['title'] ?>" data-url="<?php echo $this->url . '/' . $assign['wholeName'] . '.html' ?>"></div>
+<div class="ds-thread" data-thread-key="<?php echo $assign['wholeName'] ?>" data-title="<?php echo $assign['title'] ?>" data-url="<?php echo $this->url . '/' . $assign['wholeName'] . '.html' ?>"></div>
 <!-- 多说评论框 end -->
 <!-- 多说公共JS代码 start (一个网页只需插入一次) -->
 </div>
 <script type="text/javascript">
 var duoshuoQuery = {short_name:"pmzhang"};
-    (function() {
-        var ds = document.createElement('script');
-        ds.type = 'text/javascript';ds.async = true;
-        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-        ds.charset = 'UTF-8';
-        (document.getElementsByTagName('head')[0]
-         || document.getElementsByTagName('body')[0]).appendChild(ds);
-    })();
+(function() {
+var ds = document.createElement('script');
+ds.type = 'text/javascript';ds.async = true;
+ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+ds.charset = 'UTF-8';
+(document.getElementsByTagName('head')[0]
+|| document.getElementsByTagName('body')[0]).appendChild(ds);
+})();
 </script>
 <!-- 多说公共JS代码 end -->
-
+ */?>
 
 <?php include $this->tmplPath . '/footer.php'?>
