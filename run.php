a84cedcfc558ba57db16fc7d36f366ec450fd76a
citywill <citywill@gmail.com>
2017-09-02 18:27:28 +0800

    update

--- run.php
+++ run.php
@@ -3,7 +3,7 @@ include 'vendor/autoload.php';
 
 define('APP_PATH', dirname(__FILE__));
 
-$mark2blogConfig = array(
+$m2bConfig = array(
     'url' => 'http://www.pmzhang.com',
     'title' => 'PM老张',
     'auth' => 'pmzhang',
@@ -14,8 +14,12 @@ $mark2blogConfig = array(
     'pageSize' => 10,
 );
 
-$mark2blog = new lib\mark2blog($mark2blogConfig);
+$mark2blog = new lib\mark2blog($m2bConfig);
 $mark2blog->run();
 $generated = $mark2blog->generated;
+
+exec('cd ' . $m2bConfig['htmlPath']);
+exec('git commit -am "update"');
+exec('git push origin');
 
 echo '本次执行生成了' . $generated['article'] . '篇文章、' . $generated['index'] . '个索引页、' . $generated['rss'] . '；生成目录为' . $mark2blog->htmlPath;
